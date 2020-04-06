c7305e4b6fe0f63ee1e5ab5a7485efcfd891d809
KevinB-a <kevin.billet.pro@gmail.com>
2020-04-06 11:02:05 +0200

    first commit

---
+++ DOM/exercice1/main.js
@@ -0,0 +1,11 @@
+//Insert the main title
+var mainTitle = document.getElementById("mainTitle");
+
+//Insert the subtitle
+var subTitle = document.getElementById("subTitle")
+
+//Get all the contents elements and set a bleu background
+var contents =
+
+//Function to delete article on click
+function deleteContent()
