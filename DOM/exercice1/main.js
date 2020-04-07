8a5a9cce5e9a65adb56d75c364a1c45cbe9dc563
KevinB-a <kevin.billet.pro@gmail.com>
2020-04-07 13:59:52 +0200

    dom exo 1 finished

--- DOM/exercice1/main.js
+++ DOM/exercice1/main.js
@@ -1,11 +1,24 @@
 //Insert the main title
-var mainTitle = document.getElementById("mainTitle");
+var mainTitle = document.getElementById("mainTitle"); 
+mainTitle.textContent = "le titre";
+mainTitle.style.color = "green" // change the text color
 
 //Insert the subtitle
-var subTitle = document.getElementById("subTitle")
+var subTitle = document.getElementById("subTitle");
+subTitle.textContent = "le sous-titre";
 
-//Get all the contents elements and set a bleu background
-var contents =
-
+//Get all the contents elements and set a bleu background! 
+var contents = document.getElementsByClassName("content");
+//contents.style.backgroundColor = "blue"; // change the background color
+var firstcontent = contents[0];
+firstcontent.style.backgroundColor = "blue";
+var secondcontent = contents[1];
+secondcontent.style.backgroundColor = "blue";
+var thirdcontent = contents[2];
+thirdcontent.style.backgroundColor = "blue";
 //Function to delete article on click
 function deleteContent()
+{
+    document.querySelector("h3").remove();
+    // document.getElementById("test").remove();
+}
